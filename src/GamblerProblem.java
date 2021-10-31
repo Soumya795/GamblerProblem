faab3357ee54dd96dfa4fdb5b8565e464e2a5a1b
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-31 23:56:46 +0530

    Welcome to Gambler Simulator

---
+++ src/GamblerProblem.java
@@ -0,0 +1,12 @@
+public class GamblerProblem {
+
+    public static final double INITIAL_STAKE = 100;
+    public static final double STAKE_BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambler Simulation");
+
+    }
+
+}
+
